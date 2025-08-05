a9b4babccc87acde38e70c958d372c517b640f46
sai123-sudo <ls0122207@gmail.com>
2025-08-05 14:47:10 +0530

    hello worls started

--- catalogue.sh
+++ catalogue.sh
@@ -4,47 +4,9 @@ source ./common.sh
 app_name=catalogue
 
 check_root
+app_setup
 nodejs_setup
-
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nodejs:20"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs:20"
-
- id roboshop
- if [ $? -ne 0 ]
- then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
- else
-     echo -e "System user roboshop already created ... $Y SKIPPING $N"
- fi
-
-mkdir -p /app 
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Catalogue"
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping catalogue"
-
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing Dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue service"
-
-systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue  &>>$LOG_FILE
-systemctl start catalogue
-VALIDATE $? "Starting Catalogue"
+systemd_setup
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
@@ -58,3 +20,5 @@ then
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
+
+print_time
--- common.sh
+++ common.sh
@@ -13,6 +13,53 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+app_setup(){
+     id roboshop &>>$LOG_FILE
+ if [ $? -ne 0 ]
+ then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+ else
+     echo -e "System user roboshop already created ... $Y SKIPPING $N"
+ fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading Catalogue"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping catalogue"
+}
+
+nodejs_setup() {
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs:20"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing Dependencies"
+}
+
+systemd_setup(){
+ cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+ VALIDATE $? "Copying $app_name service"
+
+ systemctl daemon-reload &>>$LOG_FILE
+ systemctl enable $app_name  &>>$LOG_FILE
+ systemctl start $app_name
+ VALIDATE $? "Starting $app_name"
+}
+
+
 check_root(){
 if [ $USERID -ne 0 ]
 then
